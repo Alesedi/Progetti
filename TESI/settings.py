59e0eca3f52c902d638cf469aef31cfae0839639
Alesedi <alesedicina@libero.it>
2024-10-11 13:18:00 +0200

    Delete TESI/settings.py

--- TESI/settings.py
+++
@@ -1,4 +0,0 @@
-RATINGS_FILE_PATH = 'C:/Users/aless/Desktop/TESI/Server/ratings.csv'
-LINKS_FILE_PATH = 'C:/Users/aless/Desktop/TESI/Server/links.csv'
-MOVIE_FILE_PATH = 'C:/Users/aless/Desktop/TESI/Server/movies.csv'
-TAGS_FILE_PATH = 'C:/Users/aless/Desktop/TESI/Server/tags.csv'
